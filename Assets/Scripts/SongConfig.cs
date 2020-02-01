b2f2f317d05dfbebbd6b663a09d16d5bc75c9b5c
ilm353 <ian_mcdonald@rocketmail.com>
2020-01-31 22:16:43 -0600

    song config so

---
+++ Assets/Scripts/SongConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "song config", menuName = "socknic/song settings")]
+public class SongConfig : ScriptableObject
+{
+    public int bpm;
+    public float approachRate;
+    public float hp;
+    public AudioClip song;
+    public float time => song.length;
+}
