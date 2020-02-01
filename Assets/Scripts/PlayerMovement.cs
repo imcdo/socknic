cb3dd15095643aab3500b23e9dede6f319a3f23c
robertyguan <33498270+robertyguan@users.noreply.github.com>
2020-02-01 16:10:28 -0600

    working on timed jumps

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,14 +5,21 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    float bpm = RhythmManager.Instance.currentSong.bpm;
+    float bpm = 40f; //RhythmManager.Instance.currentSong.bpm;
+    float jumpHeight = 40f;//RhythmManager.Instance.jumpY - RhythmManager.Instance.targetY;
     public int m_PlayerNumber = 1; 
     Vector2 m_MovementInput;
     float m_movespeed = 10f;
     bool jumping;
-
+    float gravity = 10f;
     private void Update() {
         Move();
+        if(!jumping){
+            Fall();
+        }
+    }
+    private void Fall(){
+         m_MovementInput.y -= gravity;
     }
     private void Move(){
         Vector2 movement = new Vector2(m_MovementInput.x, 0) * m_movespeed * Time.deltaTime;
@@ -23,14 +30,13 @@ public class PlayerMovement : MonoBehaviour
         m_MovementInput.y = 0;
     }
     private void OnJump(){
-        
         if(!jumping){
             float timePassed = 0;
             while(timePassed <= bpm){
-                m_MovementInput.y += 
+                m_MovementInput.y += (jumpHeight * Time.deltaTime);
+                timePassed += Time.deltaTime;
             }
         }
-        transform.Translate(transform.up);
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
