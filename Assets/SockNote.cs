a41f7f44aaec29e283c6db245b4435237395e0da
mindietea <mindychi@gmail.com>
2020-02-01 00:12:21 -0600

    Trying to work on Rhythm manager, beats not hitting at the right time

---
+++ Assets/SockNote.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SockNote : MonoBehaviour
+{
+    public float startDsp;
+    public float endDsp;
+
+    [Header("Donut touch")]
+    public float t;
+    public float y;
+
+    void Update()
+    {
+        // Update Y position based on target
+        t = ((float) AudioSettings.dspTime - startDsp) / (endDsp - startDsp);
+        y = Mathf.Lerp(RhythmManager.Instance.spawnY,RhythmManager.Instance.targetY, t);
+        
+        transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        
+        // TODO Remove, debug code
+        if (AudioSettings.dspTime > endDsp)
+        {
+            GetComponent<AudioSource>().Play();
+            Destroy(gameObject);
+        }
+        
+    }
+}
