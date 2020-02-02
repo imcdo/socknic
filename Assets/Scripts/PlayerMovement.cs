9c353d20c05c68619455ff5c1b2428b45ed35bba
ilm353 <ian_mcdonald@rocketmail.com>
2020-02-02 03:33:09 -0600

    score prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -34,6 +34,7 @@ enum PlayerState : byte { Grounded, Jumping, Falling }
 
     [SerializeField] float _airControl = 0.5f;
     [SerializeField] int _jumpBeatMultiplier = 2;
+    [SerializeField] float _fallmultiplier = 1.1f;
     [SerializeField] int fastFall = 2;
 
     [SerializeField] private GameObject poof;
@@ -112,7 +113,7 @@ enum PlayerState : byte { Grounded, Jumping, Falling }
 //        Debug.Log($"Fooll y= {transform.position.y} T: {_activeT}");
 
         _activeT += 60 * _jumpBeatMultiplier / RhythmManager.Instance.currentSong.bpm * 
-                    Time.deltaTime * ((m_MovementInput.y < 0) ? fastFall : 1);
+                    Time.deltaTime * ((m_MovementInput.y < 0) ? fastFall : 1) * _fallmultiplier;
         if(_activeT>=1)
         {
             _startPhaseTime = (float)AudioSettings.dspTime;
@@ -120,8 +121,7 @@ enum PlayerState : byte { Grounded, Jumping, Falling }
             _activeT = 0;
             transform.position = new Vector2(transform.position.x, groundY);
 
-            Instantiate(poof, transform.position + new Vector3(-1.0f, -.4f), Quaternion.identity).transform.localScale =
-                new Vector3(.8f, .5f);
+            Instantiate(poof, transform.position + new Vector3(-1.0f, -.4f), Quaternion.identity).transform.localScale = new Vector3(.8f, .5f);
             Instantiate(poof, transform.position + new Vector3(1.0f,-.4f), Quaternion.identity).transform.localScale = new Vector2(-.8f, .5f);
             return;
         }
@@ -177,6 +177,10 @@ enum PlayerState : byte { Grounded, Jumping, Falling }
     }
     private void OnJump()
     {
+        Instantiate(poof, transform.position + new Vector3(-1.0f, -.6f), Quaternion.identity).transform.localScale = new Vector3(.4f, -.6f);
+        Instantiate(poof, transform.position + new Vector3(1.0f,-.6f), Quaternion.identity).transform.localScale = new Vector2(-.4f, -.6f);        
+        Instantiate(poof, transform.position + new Vector3(1.0f,-.6f), Quaternion.identity).transform.localScale = new Vector2(-.4f, -.6f);        
+        
         if(_playerState == PlayerState.Grounded)
         {
             _playerState = PlayerState.Jumping;
